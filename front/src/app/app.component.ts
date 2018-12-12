733311f43e12a24c5c2a10346d1fd178a97fe5ca
Batuta10 <batuta2604@gmail.com>
2018-12-12 20:48:03 -0200

    implement httpClinetModule

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'front';
+export class AppComponent implements OnInit {  
+
+    task:any;
+    constructor(private http: HttpClient) { }
+  
+    ngOnInit() {
+      this.http.get('')
+      .subscribe(x => {
+        this.task = x;
+        console.log(x);
+      })
+    }
+  
+  title = 'To-do-list';
 }
