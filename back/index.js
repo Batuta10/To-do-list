b13079e406fdf63c39c44d56c9d3df7817ffc758
Batuta10 <batuta2604@gmail.com>
2018-12-12 21:59:01 -0200

    add CORS

--- back/index.js
+++ back/index.js
@@ -12,6 +12,12 @@ app.use(helmet())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
+
 app.use('/task', task)
 
 app.get('*', (req, res) => {
