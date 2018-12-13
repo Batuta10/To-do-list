f84d593218ba900b7d05aa862d818f931bb7f506
Batuta10 <batuta2604@gmail.com>
2018-12-13 01:10:58 -0200

    indentation

--- back/app/routes/task.js
+++ back/app/routes/task.js
@@ -75,17 +75,21 @@ router.patch('/update', (req, res) => {
 
   delete req.body['id'];
 
-  Task.findOneAndUpdate({ _id: id }, { $set: req.body })
-  .exec()
-  .then(result => {
-    res.status(200).json(result);
-  })
-  .catch(err => {
-    console.log(err)
-    res.status(500).json({
-      error: err
-    });
-  })
+  Task.findOneAndUpdate({
+      _id: id
+    }, {
+      $set: req.body
+    })
+    .exec()
+    .then(result => {
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({
+        error: err
+      });
+    })
 });
 
 export default router
