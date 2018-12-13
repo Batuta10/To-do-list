ccf9ecc3e833c85f32bb55db1482876a25385f12
Batuta10 <batuta2604@gmail.com>
2018-12-13 02:27:40 -0200

    basic requirements create, delete, altere Check

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,22 +1,47 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {  
+export class AppComponent implements OnInit {
+  title = 'To-do-list';
+  tasks: any;
+  constructor(private http: HttpClient) { }
 
-    task:any;
-    constructor(private http: HttpClient) { }
-  
-    ngOnInit() {
-      this.http.get('')
+  ngOnInit() {
+    this.getTasks();
+  }
+
+  getTasks() {
+    this.http.get('http://localhost:3000/task/all')
       .subscribe(x => {
-        this.task = x;
+        this.tasks = x;
         console.log(x);
       })
-    }
-  
-  title = 'To-do-list';
+  }
+  onDelete($id: string) {
+    this.http.delete('http://localhost:3000/task/delete/' + $id)
+      .subscribe(x => {
+        console.log(x)
+        this.getTasks();
+      });
+  }
+  onAdd(x1, y2) {
+    var newTask = { titulo: x1.value, descricao: y2.value };
+    this.http.post('http://localhost:3000/task/new', newTask).subscribe(x => {
+      console.log(x);
+      this.getTasks();
+    })
+  }
+
+  alterCheck($id: string, isChecked) {
+    var upDone = { id: $id, done: !isChecked };
+    this.http.patch('http://localhost:3000/task/update', upDone).subscribe(x => {
+      console.log(x);
+      this.getTasks();
+    });
+  }
 }
