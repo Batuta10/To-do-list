cfda952d3da8427f5193beb46d4ec93be9ea6773
Batuta10 <batuta2604@gmail.com>
2018-12-13 12:48:52 -0200

    fix error TS2339

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -18,7 +18,8 @@ export class AppComponent implements OnInit {
   getTasks(status: boolean) {
     this.http.get('http://localhost:3000/task/all')
       .subscribe(x => {
-        this.tasks = x.filter(x => x.done === status);
+        this.tasks = x;
+        this.tasks = this.tasks.filter(x => x.done === status);
       })
   }
 
